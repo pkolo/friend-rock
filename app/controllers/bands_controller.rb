cf987f06266724528ba861d46046305c13e51725
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-04 10:28:04 -0400

    Set up new, create, show routes for band

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,2 +1,28 @@
 class BandsController < ApplicationController
+
+  def show
+    @band = Band.find(params[:id])
+  end
+
+  def new
+    @band = Band.new
+  end
+
+  def create
+    @band = Band.new(band_params)
+
+    if @band.save
+      log_in @band
+      redirect_to @band
+    else
+      render 'new'
+    end
+
+  end
+
+protected
+  def band_params
+    params.require(:band).permit(:email, :name, :password)
+  end
+
 end
