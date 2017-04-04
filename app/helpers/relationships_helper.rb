4f03fcb412c177729f21d8db170a3ea410b5e263
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-04 18:20:30 -0400

    Helper to verify relationship between two bands

--- app/helpers/relationships_helper.rb
+++ app/helpers/relationships_helper.rb
@@ -1,2 +1,7 @@
 module RelationshipsHelper
+
+  def relationship_exists?(band, other_band)
+    Relationship.where(band_one: band, band_two: other_band).or(Relationship.where(band_one: other_band, band_two: band)).any?
+  end
+
 end
