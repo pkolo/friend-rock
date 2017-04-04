0d7bd8eee0e47cbae9577c35160df9bb45207f04
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-04 14:56:10 -0400

    Fix unique pair validation

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,10 +3,17 @@ class Relationship < ApplicationRecord
   belongs_to :band_two, class_name: Band
   belongs_to :action_band, class_name: Band
 
-  validates :band_one, uniqueness: { scope: :band_two }
+  validate :unique_pair
   validate :cannot_add_self
 
   private
+
+    def unique_pair
+      if self.class.where("(band_one_id = :band_one_id AND band_two_id = :band_two_id) OR (band_one_id = :band_two_id AND band_two_id = :band_one_id)", {:band_one_id => band_one_id, :band_two_id => band_two_id}).exists?
+        errors.add(:band_one_id, :taken)
+      end
+    end
+
     def cannot_add_self
       errors.add(:band_two_id, 'You cannot add yourself as a friend.') if band_one_id == band_two_id
     end
