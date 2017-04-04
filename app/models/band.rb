56010ae161dc4af0d22f22cfd774be9c6cb1a813
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-04 12:17:55 -0400

    Add self-friending constraint

--- app/models/band.rb
+++ app/models/band.rb
@@ -3,4 +3,8 @@ class Band < ApplicationRecord
 
   has_many :relationships, foreign_key: :band_one_id
   has_many :more_relationships, class_name: Relationship, foreign_key: :band_two_id
+
+  def all_relationships
+    (self.relationships + self.more_relationships)
+  end
 end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,4 +4,10 @@ class Relationship < ApplicationRecord
   belongs_to :action_band, class_name: Band
 
   validates :band_one, uniqueness: { scope: :band_two }
+  validate :cannot_add_self
+
+  private
+    def cannot_add_self
+      errors.add(:band_two_id, 'You cannot add yourself as a friend.') if band_one_id == band_two_id
+    end
 end
