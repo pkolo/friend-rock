c06f3a87fd28f44086fb7cb23b3391e6c16c8422
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-04 10:33:25 -0400

    Add session helper methods

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,20 @@
+module SessionsHelper
+
+  def log_in(band)
+    session[:band_id] = band.id
+  end
+
+  def current_band
+    @current_band ||= band.find_by(id: session[:band_id])
+  end
+
+  def logged_in?
+    !current_band.nil?
+  end
+
+  def log_out
+    session.delete(:band_id)
+    @current_band = nil
+  end
+
+end
