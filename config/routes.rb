0cd8c83cd22fa35c0b551fe48dd5fafd123220ec
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-04 17:02:31 -0400

    Add routes for relationship actions

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
+  get '/send_request/:id', to: 'relationships#send_request', :as => "send_request"
+  get '/accept_request/:id', to: 'relationships#accept_request', :as => "accept_request"
+
   resources :bands, only: [:show, :create]
 
 end
