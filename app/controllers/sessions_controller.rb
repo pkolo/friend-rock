3363a881f749a3b93747ab7a9570413f3763dfe0
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-04 10:48:53 -0400

    Create and destroy actiosn for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+  
+  def create
+    band = Band.find_by(email: params[:sesion][:email])
+    if band && band.authenticate(params[:session][:password])
+      log_in band
+      redirect_to band
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
 end
