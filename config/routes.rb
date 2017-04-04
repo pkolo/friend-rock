54e6790e5a7f55ec831dc2449b6fb8e24acf79e2
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-04 10:51:27 -0400

    Update routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :bands, only: [:show, :new, :create]
+  root 'sessions#new'
+
+  get '/register', to: 'bands#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  resources :bands, only: [:show, :create]
+
 end
