269cb657638309af8dee2eb3737faa0400444bbf
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-08 18:10:00 -0400

    Create locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408220626) do
+ActiveRecord::Schema.define(version: 20170408220807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20170408220626) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest", null: false
     t.index ["name"], name: "index_bands_on_name", using: :btree
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "city",       null: false
+    t.string   "state",      null: false
+    t.string   "country",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "relationships", force: :cascade do |t|
