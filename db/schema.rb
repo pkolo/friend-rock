2a199a7137a8f3eaf12abf6fec363cd6cdc3ed05
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-08 18:11:29 -0400

    Add location reference to bands table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408220807) do
+ActiveRecord::Schema.define(version: 20170408221018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170408220807) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest", null: false
+    t.integer  "location_id"
+    t.index ["location_id"], name: "index_bands_on_location_id", using: :btree
     t.index ["name"], name: "index_bands_on_name", using: :btree
   end
 
