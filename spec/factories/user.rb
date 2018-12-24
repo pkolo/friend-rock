f29ba24391cb22eaa2189a8fdd7f6c2839e9b63b
pkolo <pkolodgy@gmail.com>
2018-12-24 09:01:15 -0500

    Create tests for user registration

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,9 @@ FactoryBot.define do
     username { Faker::Lorem.word }
     email { Faker::Internet.email }
     password { Faker::Internet.password(10, 20, true, true) }
+
+    trait :verified_user do
+      after(:create) { |user| user.verify_email }
+    end
   end
 end
